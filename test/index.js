dfe076032ffa618de56bada440f3698686d95108
Mariusz Nowak <mariusz@medikoo.com>
2012-08-06 17:59:18 +0200

    Test emitters that span prototype chain

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function (t, a) {
-	var x = t(), y, count, count2, test, listener1, listener2;
+	var x = t(), y, count, count2, count3, count4, test, listener1, listener2;
 
 	x.emit('none');
 
@@ -53,4 +53,59 @@ module.exports = function (t, a) {
 	x.off('foo', listener1);
 	x.emit('foo');
 	a(count, 0, "Once Off: Not emitted");
+
+	count = 0;
+	x.on('foo', listener2 = function () {});
+	x.once('foo', listener1 = function (x, y, z) {
+		++count;
+	});
+
+	x.off('foo', listener1);
+	x.emit('foo');
+	a(count, 0, "Once Off (multi): Not emitted");
+	x.off('foo', listener2);
+
+	test = "Prototype Share: ";
+
+	y = Object.create(x);
+
+	count = 0;
+	count2 = 0;
+	count3 = 0;
+	count4 = 0;
+	x.on('foo', function () {
+		++count;
+	});
+	y.on('foo', function () {
+		++count2;
+	});
+	x.once('foo', function () {
+		++count3;
+	});
+	y.once('foo', function () {
+		++count4;
+	});
+	x.emit('foo');
+	a(count, 1, test + "x on count");
+	a(count2, 0, test + "y on count");
+	a(count3, 1, test + "x once count");
+	a(count4, 0, test + "y once count");
+
+	y.emit('foo');
+	a(count, 1, test + "x on count");
+	a(count2, 1, test + "y on count");
+	a(count3, 1, test + "x once count");
+	a(count4, 1, test + "y once count");
+
+	x.emit('foo');
+	a(count, 2, test + "x on count");
+	a(count2, 1, test + "y on count");
+	a(count3, 1, test + "x once count");
+	a(count4, 1, test + "y once count");
+
+	y.emit('foo');
+	a(count, 2, test + "x on count");
+	a(count2, 2, test + "y on count");
+	a(count3, 1, test + "x once count");
+	a(count4, 1, test + "y once count");
 };
